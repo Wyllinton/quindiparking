827371cee1508c2089364c90e5a8ce0b5c5c2aa0
storm <miguela.vargasp@uqvirtual.edu.co>
2026-03-07 13:08:34 -0500

    Verificacion

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
+
 const routes: Routes = [
   {
     path: '',
