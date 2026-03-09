12ee6a2e2206eef1e5ebf3d70f91879b019777c1
storm <miguela.vargasp@uqvirtual.edu.co>
2026-03-09 00:21:55 -0500

    Listo para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.quindiparking.com/v1',
+  apiUrl: 'http://16.59.147.16:8080/api',
   googleClientId: '1041116492056-524uv0k0svvt85teq42dqe0j9feu44fa.apps.googleusercontent.com',
   recaptchaSiteKey: '6Lfl8IEsAAAAACtbdogdy_4gz80y2Lk8dl3Yr308'
 };
