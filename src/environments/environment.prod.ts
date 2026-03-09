817297feae7105800fc2e08ee65b07de479e958e
storm <miguela.vargasp@uqvirtual.edu.co>
2026-03-09 03:06:10 -0500

    Listo para produccion
    Signed-off-by: storm <miguela.vargasp@uqvirtual.edu.co>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://16.59.147.16:8080/api/v1',
+  apiUrl: 'https://api.quindiparking.online/api/v1',
   googleClientId: '1041116492056-524uv0k0svvt85teq42dqe0j9feu44fa.apps.googleusercontent.com',
   recaptchaSiteKey: '6Lfl8IEsAAAAACtbdogdy_4gz80y2Lk8dl3Yr308'
 };
-
