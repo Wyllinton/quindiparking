393c1098f53a7753a0ece63c2b76bde9449239a4
storm <miguela.vargasp@uqvirtual.edu.co>
2026-04-18 00:31:36 -0500

    actualizar ambiente
    Signed-off-by: storm <miguela.vargasp@uqvirtual.edu.co>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://api.quindiparking.online/api/v1',
   googleClientId: '1041116492056-524uv0k0svvt85teq42dqe0j9feu44fa.apps.googleusercontent.com',
   recaptchaSiteKey: '6Lfl8IEsAAAAACtbdogdy_4gz80y2Lk8dl3Yr308'
 };
